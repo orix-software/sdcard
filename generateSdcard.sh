20c38b41d7cf79388a90c8817ea82a2b61312a72
jede <jede@oric.org>
2020-05-22 00:29:28 +0200

    Don't generate sdcard already generated

--- generateSdcard.sh
+++ generateSdcard.sh
@@ -3,6 +3,7 @@
 wget -r --no-parent http://repo.orix.oric.org/dists/2020.2/tgz/6502/
 mkdir -p sdcard
 mv repo.orix.oric.org/dists/2020.2/tgz/6502/*.tgz sdcard/
+rm sdcard/sdcard.tgz
 cd sdcard && gzip -d *.tgz && for I in `ls *.tar`; do tar xvf $I; done && rm *.tar
 
 
