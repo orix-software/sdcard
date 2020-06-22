f3429efa5cdca1cde79f24e3de8a3d4cd7491b1d
jede <jede@oric.org>
2020-06-22 16:26:55 +0200

    update to official

--- generateSdcard.sh
+++ generateSdcard.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-wget -r --no-parent http://repo.orix.oric.org/dists/2020.2/tgz/6502/
+wget -r --no-parent http://repo.orix.oric.org/dists/official/tgz/6502/
 mkdir -p sdcard
-mv repo.orix.oric.org/dists/2020.2/tgz/6502/*.tgz sdcard/
+mv repo.orix.oric.org/dists/official/tgz/6502/*.tgz sdcard/
 rm sdcard/sdcard.tgz
 cd sdcard && gzip -d *.tgz && for I in `ls *.tar`; do tar xvf $I; done && rm *.tar
 
