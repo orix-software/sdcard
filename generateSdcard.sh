c9d10996a06e6c3fd1bfb320fbf16bbc5bca9b2b
jedeoric <plifplouf78@hotmail.com>
2022-06-23 13:34:32 +0200

    Update generateSdcard.sh

--- generateSdcard.sh
+++ generateSdcard.sh
@@ -5,6 +5,3 @@ mkdir -p build
 mv repo.orix.oric.org/dists/official/tgz/6502/*.tgz build/
 rm build/sdcard.tgz
 cd build && gzip -d *.tgz && for I in `ls *.tar`; do tar xvf $I; done && rm *.tar
-
-
-
